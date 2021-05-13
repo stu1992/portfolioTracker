1a45f19e49944ced833ffad291dc0f1f19566c98
Stu May <stumay1992@gmail.com>
2021-05-13 17:28:53 +1000

    Very trashy code but I'm able to register users manually and I'm going to implement authentication, just don't want to lose this state

--- src/backend/routes/Portfolio.js
+++ src/backend/routes/Portfolio.js
@@ -1,11 +1,26 @@
 
 const router = require('express').Router();
 let Portfolio = require('../model/Portfolio');
+const User = require('../model/User');
 
 router.route('/:id').get((req, res) => {
 Portfolio.findById(req.params.id)
 .then(portfolio => res.json(portfolio))
 .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/register').post((req, res) => {
+  //const {email, password } = req.body;
+  const userdata = {"email": "Kiana", "password": "password"}
+  console.log(req.body);
+  const user = new User(userdata);
+  user.save(function(err) {
+    if (err) {
+      res.status(500).json("error registering user1");
+    }else{
+      res.status(200).json("done");
+    }
+    })
 });
 
 
