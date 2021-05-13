fd3dfd3cfee9df2c319b6f9d22ed709c8b1d424d
Stu May <stumay1992@gmail.com>
2021-05-13 10:00:04 +1000

    Initial boilerplate commit of a multi page router app

--- src/App.js
+++ src/App.js
@@ -1,80 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 import Header from './components/Header'
-import Stocks from './components/Stocks'
-import Chart from './components/Chart'
-import LandingPage from './components/LandingPage'
-import { useState , useEffect } from 'react'
-import { render } from 'react-dom'
-
-function App() {
-
-  function fetchUser(){
-    const getStocks = async () => {
-      const JsonFromServer = await fetchStocks()
-      setStocks(JsonFromServer.seriesdataset)
-      setDates(JsonFromServer.dates)
-      setPortfolio(JsonFromServer.portfolio)
-    }
-    getStocks()
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={Header} />
+          </Switch>
+        </div>
+      </Router>
+    );
   }
-  useEffect(() =>{fetchUser()}, [])
-
-  const basePortfolioURI = 'http://localhost:7000/Portfolio/'
-  var portfolioURI = basePortfolioURI
-
-  const fetchStocks = async() => {
-    const res = await fetch (portfolioURI)
-    const data = await res.json()
-    console.log(data)
-    return data
-  }
-
-  const [stocks, setStocks] = useState([
-]);
-
-const [dates, setDates] = useState([
-]);
-
-const [portfolio, setPortfolio] = useState([
-]);
-
-  function StuPortfolio(){
-    portfolioURI = basePortfolioURI+'Stu'
-    fetchUser()
-  }
-
-  function KianasPortfolio(){
-    portfolioURI = basePortfolioURI+'Kiana'
-    fetchUser()
-  }
-
-const search = window.location.search;
-const params = new URLSearchParams(search);
-const user = params.get('user');
-
-if (user != null){
-  portfolioURI = basePortfolioURI+user;
-  return (
-    <div className="App">
-            <Chart stocks={stocks} dates={dates}/>
-              <Stocks stocks={stocks} portfolio={portfolio} />
-    </div>
-  );
-}else{
-  return (
-    <div className="App">
-      <ul>
-  <li>Want to be rich</li>
-  <li>Invest. Dont speculate</li>
-  <li>Gamify</li>
-  <li>The system isn't rigged, your psychology is</li>
-</ul>
-
-      <LandingPage />
-    </div>
-  );
-}
 }
 
 export default App;
