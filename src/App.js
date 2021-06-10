7fbff5d28ef34af9376c030df52fa76909f4f296
Stu May <stumay1992@gmail.com>
2021-06-10 18:34:03 +1000

    some basic ui updates

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 
   constructor() {
   super();
+  document.title = "Making My Mates rich";
   this.state = {
     loggedin: false,
     titles: {index: "Our Mission"}, // This is just variables for this page, simple words that change
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import marketImg from '../market.png';
 import loggedInMarketImg from '../private_market.png';
+import FittedImg from 'react-fitted-img';
 const LandingPage = ({state}) => {
   if( state['email']){
 
-    return <img style={{width:'100%'}} src={loggedInMarketImg} alt="Logo" />;
-
+    return (
+      <div>
+        <img style={{maxWidth:'100%'}} src={loggedInMarketImg} alt="Market trends" />
+     </div>
+    )
     }else{
-   return <img style={{width:'100%'}} src={marketImg} alt="Logo" />;
+   return(
+     <div>
+    <img style={{maxWidth:'100%'}} src={marketImg} alt="Market trends" />
+</div>
+  )
    }
-};
+}
 export default LandingPage;
