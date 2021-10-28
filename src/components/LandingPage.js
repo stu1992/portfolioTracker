55385e14e8d41acb5b7227ed817d527a4c0c604f
Stu May <stumay1992@gmail.com>
2021-10-28 22:12:42 +1100

    wip: cycle through graphs

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,11 +2,42 @@ import Image from 'react';
 import marketImg from '../market.png';
 import loggedInMarketImg from '../private_market.png';
 import FittedImg from 'react-fitted-img';
+import Button from './elements/Button';
+import React, { useRef, useEffect, Component } from 'react';
+
 
 const LandingPage = ({dailySecret}) => {
+
+
+  const [secret, setSecret] = React.useState(dailySecret);
+
+  const scope = ["0", "6", "3", "1"];
+  var currentScope = 0;
+  function changeScope(){
+    var currentStr = scope[currentScope];
+    console.log("the index is " + currentScope);
+    var newStr = "";
+    if(currentScope == 3){
+      newStr = scope[0]
+      currentScope = 0;
+      console.log(currentStr);
+      console.log(newStr);
+    }else {
+      currentScope ++;
+      newStr = scope[currentScope]
+      console.log(currentStr);
+      console.log(newStr);
+    }
+    console.log(secret);
+    var newDailySecret = secret.replace(currentStr, newStr);
+    console.log(newDailySecret);
+    dailySecret = newDailySecret;
+    setSecret(dailySecret);
+  }
+
     return (
       <div>
-        <img style={{maxWidth:'100%', margin: "auto"}} src={dailySecret} alt="Market trends" />
+        <img style={{maxWidth:'100%', margin: "auto"}} onClick={changeScope} src={secret} alt="Market trends" />
      </div>
     )
 }
