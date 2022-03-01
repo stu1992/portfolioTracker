374c6aa930e869aec5cdc8f9e4830a3680030f92
Stu May <stumay1992@gmail.com>
2022-03-01 22:49:18 +1100

    chore: add signup button

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -120,7 +120,7 @@ const Header = ({
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        <Link to="/Signup" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
                       </li>
                     </ul>}
                 </div>
